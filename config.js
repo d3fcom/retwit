754f4e72a5512233f59ed330aaf8efa5f729c5d2
d3fcom <weis23.lp@gmail.com>
2018-09-20 22:40:52 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'blablablablablablabla',
+  consumer_secret:      'blablablablablablabla',
+  access_token:         'blablablablablablabla',
+  access_token_secret:  'blablablablablablabla'
+}
